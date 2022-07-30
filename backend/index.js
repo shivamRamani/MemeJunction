f361537f11adce1a934dd6375e95f4a41d336a27
shivam <shivamramani40@gmail.com>
2022-07-30 21:05:13 +0530

    warning update

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
     });
